428479f0d82f355063bdea7d7a906a4dda48251a
vapormusic <vietanhfat@gmail.com>
2021-05-08 17:02:32 +0700

    functioning lazy loading chapter list

--- pip-get.py
+++ pip-get.py
@@ -1,50 +1,84 @@
 import sys
-from PyQt4 import QtGui, QtCore
-from PyQt4.QtCore import Qt
+import time
+from PyQt4 import QtCore, QtGui
+from PyQt4.QtCore import pyqtSignal, pyqtSlot
+import threading
 
 
-class TestListModel(QtCore.QAbstractListModel):
+def logthread(caller):
+    print('%-25s: %s, %s,' % (caller, threading.current_thread().name,
+                              threading.current_thread().ident))
+
+
+class MyApp(QtGui.QWidget):
+
     def __init__(self, parent=None):
-        QtCore.QAbstractListModel.__init__(self, parent)
-        self.list = parent
+        QtGui.QWidget.__init__(self, parent)
 
-    def rowCount(self, index):
-        return 1000
+        self.setGeometry(300, 300, 280, 600)
+        self.setWindowTitle('using threads')
 
-    def data(self, index, role):
-        if role == Qt.DisplayRole:
-            if not self.list.indexWidget(index):
-                button = QtGui.QPushButton("This is item #%s" % index.row())
-                self.list.setIndexWidget(index, button)
-                button.setVisible(False)
-            return QtCore.QVariant()
+        self.layout = QtGui.QVBoxLayout(self)
 
-        if role == Qt.SizeHintRole:
-            return QtCore.QSize(100, 50)
+        self.testButton = QtGui.QPushButton("QThread")
+        self.testButton.released.connect(self.test)
+        self.listwidget = QtGui.QListWidget(self)
 
-    def columnCount(self, index):
-        pass
+        self.layout.addWidget(self.testButton)
+        self.layout.addWidget(self.listwidget)
+
+        self.threadPool = []
+        logthread('mainwin.__init__')
+
+    def add(self, text):
+        """ Add item to list widget """
+        logthread('mainwin.add')
+        self.listwidget.addItem(text)
+        self.listwidget.sortItems()
+
+    def addBatch(self, text="test", iters=6, delay=0.3):
+        """ Add several items to list widget """
+        logthread('mainwin.addBatch')
+        for i in range(iters):
+            time.sleep(delay)  # artificial time delay
+            self.add(text+" "+str(i))
+
+    def test(self):
+        my_thread = QtCore.QThread()
+        my_thread.start()
+
+        # This causes my_worker.run() to eventually execute in my_thread:
+        my_worker = GenericWorker(self.addBatch)
+        my_worker.moveToThread(my_thread)
+        my_worker.start.emit("hello")
+        # my_worker.finished.connect(self.xxx)
+
+        self.threadPool.append(my_thread)
+        self.my_worker = my_worker
 
 
-def main():
-    app = QtGui.QApplication(sys.argv)
+class GenericWorker(QtCore.QObject):
 
-    window = QtGui.QWidget()
+    start = pyqtSignal(str)
+    finished = pyqtSignal()
 
-    list = QtGui.QListView()
-    model = TestListModel(list)
+    def __init__(self, function, *args, **kwargs):
+        super(GenericWorker, self).__init__()
+        logthread('GenericWorker.__init__')
+        self.function = function
+        self.args = args
+        self.kwargs = kwargs
+        self.start.connect(self.run)
 
-    list.setModel(model)
-    list.setVerticalScrollMode(QtGui.QAbstractItemView.ScrollPerPixel)
-
-    layout = QtGui.QVBoxLayout(window)
-    layout.addWidget(list)
-
-    window.setLayout(layout)
-    window.show()
-
-    sys.exit(app.exec_())
+    @pyqtSlot()
+    def run(self, *args, **kwargs):
+        logthread('GenericWorker.run')
+        self.function(*self.args, **self.kwargs)
+        self.finished.emit()
 
 
-if __name__ == '__main__':
-    main()
+# run
+app = QtGui.QApplication(sys.argv)
+test = MyApp()
+test.show()
+app.exec_()
